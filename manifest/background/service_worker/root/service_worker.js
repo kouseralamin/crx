163e76be325352dccc59e0507f1752c0c7a5886f
kouseralamin <kouseralamin@gmail.com>
2022-10-09 21:02:44 +0600

    added: download notifier

--- manifest/background/service_worker/root/service_worker.js
+++ manifest/background/service_worker/root/service_worker.js
@@ -7,7 +7,24 @@ chrome.webRequest.onErrorOccurred.addListener(
                 iconUrl: "/blob/images/photo.jpg",
                 title: (typeof details.error === "undefined") ? "net::ERROR" : details.error,
                 message: (typeof details.initiator === "undefined") ? "net::ERROR" : details.initiator,
-            });
+            }
+        );
     },
     { urls: ["<all_urls>"] }
+);
+
+chrome.downloads.onChanged.addListener(
+    function (details) {
+        if (typeof details.state !== "undefined" && details.state.current === "complete") {
+            chrome.notifications.create(
+                Math.random().toString(),
+                {
+                    type: "basic",
+                    iconUrl: "/blob/images/photo.jpg",
+                    title: "DOWNLOAD STATUS",
+                    message: "A FILE DOWNLOADED"
+                }
+            )
+        }
+    },
 );
