8f2e6664d5319e29b7afd2e1148ce522bbe7894a
kouseralamin <kouseralamin@gmail.com>
2022-10-09 15:34:11 +0600

    fixed: notification

--- manifest/background/service_worker/root/service_worker.js
+++ manifest/background/service_worker/root/service_worker.js
@@ -1,12 +1,14 @@
 chrome.webRequest.onErrorOccurred.addListener(
     function (details) {
         console.log(details);
-        chrome.notifications.create({
-            type: "basic",
-            iconUrl: "/blob/images/photo.jpg",
-            title: details.error,
-            message: details.initiator,
-        });
+        chrome.notifications.create(
+            Math.random().toString(),
+            {
+                type: "basic",
+                iconUrl: "/blob/images/photo.jpg",
+                title: details.error,
+                message: details.initiator,
+            });
     },
     { urls: ["<all_urls>"] }
 );
