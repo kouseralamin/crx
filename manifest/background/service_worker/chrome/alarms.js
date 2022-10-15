55c98740ce480832f9c930e1547adbe77b4f3de8
kouseralamin <kouseralamin@gmail.com>
2022-10-15 12:10:25 +0600

    run every minute

--- manifest/background/service_worker/chrome/alarms.js
+++ manifest/background/service_worker/chrome/alarms.js
@@ -1,12 +1,12 @@
 chrome.alarms.clearAll();
 
-chrome.alarms.create("clear_storage", {
+chrome.alarms.create("ONE_MINUTE", {
     delayInMinutes: 0,
-    periodInMinutes: 0.20
+    periodInMinutes: 1
 });
 
 chrome.alarms.onAlarm.addListener(function (alarm) {
-    if (alarm.name === "clear_storage") {
+    if (alarm.name === "ONE_MINUTE") {
         chrome.downloads.search({}, function (downloads) {
             downloads.forEach(download => {
                 if (download.state !== "in_progress") {
