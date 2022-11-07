ca6e55661338e6891fa038437f4cffa38a9c9495
kouseralamin <kouseralamin@gmail.com>
2022-11-07 11:18:09 +0600

    fixes

--- manifest/chrome_url_overrides/newtab/scripts/weather.js
+++ manifest/chrome_url_overrides/newtab/scripts/weather.js
@@ -2,8 +2,8 @@ async function weather() {
     return fetch(SECRET.weather).then((value) => {
         return value.text();
     }).then((value) => {
-        window.localStorage.setItem("weather", value);
-        return value;
+        window.localStorage.setItem("weather", value.replace(/  +/g, " "));
+        return value.replace(/  +/g, " ");
     }).catch((_error) => {
         return window.localStorage.getItem("weather");
     })
