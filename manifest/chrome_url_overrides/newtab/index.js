4046d7445b5b218115e99846888576af817519c9
kouseralamin <kouseralamin@gmail.com>
2022-11-09 00:33:55 +0600

    added: topSites, history

--- manifest/chrome_url_overrides/newtab/index.js
+++ manifest/chrome_url_overrides/newtab/index.js
@@ -8,6 +8,21 @@ time().then((value) => {
     toast(Math.abs(parseInt(value) - new Date().getTime()), 10000);
 })
 
+chrome.topSites.get(
+    function (topSites) {
+        const topSitesNode = window.document.getElementById("topSites").getElementsByTagName("details")[0].getElementsByTagName("ul")[0];
+        topSites.forEach(element => {
+            const topSite = window.document.createElement("li");
+            const topSiteAnchor = window.document.createElement("a");
+            const topSiteTitle = window.document.createTextNode(element.title);
+            topSiteAnchor.appendChild(topSiteTitle);
+            topSiteAnchor.href = element.url;
+            topSite.appendChild(topSiteAnchor);
+            topSitesNode.appendChild(topSite);
+        });
+    }
+)
+
 chrome.bookmarks.getTree(
     function (bookmarksTree) {
         const bookmarksFolders = window.document.getElementById("bookmarks").getElementsByTagName("details")[0];
@@ -33,17 +48,32 @@ chrome.bookmarks.getTree(
     }
 )
 
-chrome.topSites.get(
-    function (topSites) {
-        const topSitesNode = window.document.getElementById("topSites").getElementsByTagName("details")[0].getElementsByTagName("ul")[0];
-        topSites.forEach(element => {
-            const topSite = window.document.createElement("li");
-            const topSiteAnchor = window.document.createElement("a");
-            const topSiteTitle = window.document.createTextNode(element.title);
-            topSiteAnchor.appendChild(topSiteTitle);
-            topSiteAnchor.href = element.url;
-            topSite.appendChild(topSiteAnchor);
-            topSitesNode.appendChild(topSite);
-        });
+window.document.getElementById("history").getElementsByTagName("input")[0].addEventListener("keyup", function (_) {
+    const text = window.document.getElementById("history").getElementsByTagName("input")[0].value;
+    const historyItemNode = window.document.getElementById("history").getElementsByTagName("div")[0];
+    if (text === "") {
+        historyItemNode.innerHTML = "";
+    } else {
+        chrome.history.search(
+            {
+                maxResults: 0,
+                text: text
+            },
+            function (HistoryItem) {
+                historyItemNode.innerHTML = "";
+                console.log(HistoryItem);
+                if (HistoryItem.length === 0) {
+                    historyItemNode.innerText = "No Item Found."
+                } else {
+                    HistoryItem.forEach(element => {
+                        const history = window.document.createElement("a");
+                        const historyTitle = window.document.createTextNode(`${element.title + "\n" + element.url}`);
+                        history.href = element.url;
+                        history.appendChild(historyTitle);
+                        historyItemNode.appendChild(history);
+                    });
+                }
+            }
+        )
     }
-)
+})
