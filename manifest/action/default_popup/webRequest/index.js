0cda840553a3a9fb0219614b6dea3edafdc8ad76
kouseralamin <kouseralamin@gmail.com>
2022-11-09 13:40:45 +0600

    added: download webRequest

--- manifest/action/default_popup/webRequest/index.js
+++ manifest/action/default_popup/webRequest/index.js
@@ -1,3 +1,6 @@
+"use strict";
+
+const EVENTS = [];
 [
     "onBeforeRequest",
     "onBeforeSendHeaders",
@@ -12,6 +15,7 @@
     chrome.webRequest[event].addListener(
         function (details) {
             details.event = event;
+            EVENTS.push(details);
             const webRequest = window.document.createElement("div");
             Object.keys(details).sort().forEach(element => {
                 const webRequestInfo = window.document.createElement("div");
@@ -19,9 +23,17 @@
                 webRequestInfo.appendChild(webRequestInfoText);
                 webRequest.appendChild(webRequestInfo);
             });
-            window.document.body.appendChild(webRequest);
+            window.document.getElementById("events").appendChild(webRequest);
             window.scrollTo(0, document.body.scrollHeight);
         },
         { urls: ["<all_urls>"] }
     );
 });
+
+window.document.getElementById("download").onclick = function () {
+    const blob = new Blob([JSON.stringify(EVENTS)], { type: "application/json" });
+    const link = document.createElement('a');
+    link.href = window.URL.createObjectURL(blob);
+    link.download = "webRequest.json";
+    link.click();
+}
