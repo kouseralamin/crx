f83d8384487de659fd59f85e176d55b9ef5390b9
kouseralamin <kouseralamin@gmail.com>
2022-11-09 12:12:32 +0600

    added: Open In Window

--- manifest/action/default_popup/index.js
+++ manifest/action/default_popup/index.js
@@ -1,3 +1,9 @@
-window.document.getElementById("webRequest").onclick = function() {
+window.document.getElementById("webRequest").onclick = function () {
     window.open("./webRequest/index.html", "_blank", "width=500,height=500");
+}
+
+window.document.getElementById("openInWindow").onclick = function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        window.open(tabs[0]["url"], "_blank", "width=500,height=500");
+    });
 }
